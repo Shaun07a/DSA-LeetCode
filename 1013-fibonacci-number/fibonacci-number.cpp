5ebcb2528ecb63a7d319ec2dd8fec5618760fcd3
Shaun Joseph <shaunjoseph004@gmail.com>
2026-07-05 22:34:13 +0530

    Time: 6 ms (53.49%) | Memory: 7.7 MB (88.35%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fib(int n) {
+        if ( n <= 1){
+            return n;
+        }
+
+        int first = fib(n - 1);
+        int last = fib(n - 2);
+
+        return first + last;
+    }
+};
