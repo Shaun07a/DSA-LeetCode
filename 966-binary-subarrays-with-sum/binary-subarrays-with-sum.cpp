76c0de82ebd0c43299da4c4cf2005cd01f155a1c
Shaun Joseph <shaunjoseph004@gmail.com>
2026-08-14 14:41:48 +0530

    Time: 0 ms (100.00%) | Memory: 32.7 MB (40.65%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int atmost(vector<int> &nums, int k){
+        if(k < 0) return 0;
+        int left = 0, sum = 0, cnt = 0;
+        for(int right = 0; right < nums.size(); right++){
+            sum += nums[right];
+
+            while(sum > k){
+                sum -= nums[left];
+                left++;
+            }
+
+            cnt += right - left + 1;
+        }
+
+        return cnt;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return atmost(nums, goal) - atmost(nums, goal - 1);
+    }
+};
